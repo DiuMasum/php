7d11c48ca4e47bd1d90c787b39d103a266991e2b
DiuMasum <masum35-577@gmail.com>
2024-01-17 18:17:03 +0600

    variavle and data types

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+    $name = "masum";
+    $age = 23;
+    $food = "biriyani";
+    $cgpa = 3.50;
+    $online = true;
+    $forSale = false;
+
+    echo"Hello my name is {$name} <br>";
+    echo"your are {$age} years old <br>";
+    echo"and i thik you like {$food} so much haa! <br>";
+    echo"your cgps is {$cgpa}<br>";
+    echo"your now online {$online}<br>";
+    echo"is this for sale {$forSale}";
+?>
