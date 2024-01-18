b42e2c9f8cccbd4157f2013c642c6a05c1b45b25
DiuMasum <masum35-577@gmail.com>
2024-01-19 00:49:08 +0600

    switch

---
+++ switch.php
@@ -0,0 +1,28 @@
+<?php
+    // example 1
+    $grade = "c";
+
+    switch($grade){
+        case "a":
+            echo "you got A";
+            break;
+        case "b":
+            echo "you got B";
+            break;
+        case "c":
+            echo "you got C";
+            break;
+        case "d":
+            echo "you got D";
+            break;
+
+        default:
+            echo "not a valid";
+            break;
+    }
+
+
+
+    // example 2
+
+?>
