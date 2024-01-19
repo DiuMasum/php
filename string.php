45ce3cd7b09db67819ae99a1a4ca91ccb2fe3600
DiuMasum <masum35-577@gmail.com>
2024-01-20 01:45:20 +0600

    string functions

---
+++ string.php
@@ -0,0 +1,18 @@
+<?php
+    $username = "masum sahriar";
+    $phone = "434-34-242";
+
+    //$username = strtolower($username);
+    //$username = strtoupper($username);
+    //$username = trim($username);
+    //$username = str_pad($username, 20, "0");
+    //$phone = str_replace("-", "/", $phone);
+    //$username = strrev($username);
+    //$username = substr($username, 0, 3);
+    //$username = substr($username, 4);
+    //$username = explode(" ", $username);
+    //$username = implode("-", $username);
+
+    echo $username . "<br>";
+    echo $phone;
+?>
