68ef948a2c139341a674c2ec486abc6cf2e63ebe
DiuMasum <masum35-577@gmail.com>
2024-01-19 12:26:48 +0600

    array

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+    $foods = array("apple", "banana", "lemon");
+
+    //$foods[0] = "lichi";
+    //array_push($foods, "water melon");
+    //array_pop($foods);
+    //array_shift($foods);
+    //echo count($foods);
+
+
+    foreach($foods as $food){
+        echo $food . "<br>";
+    }
+?>
