e4c951a48bbe62cd572fc392d3c5099f01253f90
DiuMasum <masum35-577@gmail.com>
2024-01-19 22:57:52 +0600

    radio buttons

---
+++ radioButtons.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="radioButtons.php">
+        <input type="radio" name="credit_card" value="visa">
+        Visa <br>
+        <input type="radio" name="credit_card" value="mastercard">
+        Mastercard <br>
+        <input type="radio" name="credit_card" value="americanexpress">
+        American express <br>
+        <input type="submit" name="confirm" value="confirm">
+    </form>
+</body>
+</html>
+
+<?php
+    if(isset($_POST["confirm"])){
+
+        $credit_card = null;
+
+        if(isset($_POST["credit_card"])){
+            $credit_card = $_POST["credit_card"];
+        }
+
+        switch($credit_card){
+            case "visa":
+                echo "you are selected visa";
+                break;
+            case "mastercard":
+                echo "you are selected Mastercard";
+                break;
+            case "americanexpress":
+                echo "you are selected American express";
+                break;
+            default:
+                echo "please select at least one";
+        }
+    }
+?>
