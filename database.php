b1a32329fa79bb94120dbdb558e2ea5c8c5b5f26
DiuMasum <masum35-577@gmail.com>
2024-01-20 14:25:44 +0600

    php connect to MySQL database

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+    $db_server = "localhost";
+    $bd_user = "root";
+    $bd_pass = "";
+    $bd_name = "businessbd";
+    $conn = "";
+
+    if($conn){
+        echo "you are connected";
+    }
+    else{
+        echo "you are not connected";
+    }
+?>
